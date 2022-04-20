52661af9f2743ef9dc7303a9d1f8c6a5c472b8cb
kyungmin <39684855+skm0626@users.noreply.github.com>
2022-04-20 11:26:36 +0900

    Create week2_1.py

---
+++ Week2/week2_1.py
@@ -0,0 +1,27 @@
+#콤마로 출력하면 띄어쓰기가 되어서 출력됨
+#숫자의 연산
+a=1
+b=5
+print(a+b)
+
+c=a-b
+print(c)
+
+c=b%2
+print(c)
+
+c=b //2
+print(c)
+
+#문자의 연산
+a="Hello"
+b="World"
+print(a+b)
+print(a*3)
+
+#문자와 숫자 더하기
+
+a=100
+a=str(a)
+b="원"
+print(a+b)
